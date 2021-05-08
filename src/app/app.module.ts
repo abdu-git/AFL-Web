79f12dcf62db9a0c0f943497c6a95beb2dd6e120
Abdullah Alwahaibi <19574962@students.ltu.edu.au>
2021-05-08 21:50:39 +0400

    http module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ViewLeagueTableComponent } from './view-league-table/view-league-table.component';
@@ -12,7 +12,8 @@ import { ViewLeagueTableComponent } from './view-league-table/view-league-table.
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
