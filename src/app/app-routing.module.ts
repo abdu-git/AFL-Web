9ee4f03da0b1e8383f6e7edded35352dbd24f996
Abdullah Alwahaibi <19574962@students.ltu.edu.au>
2021-05-08 22:54:13 +0400

    app routing update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ViewLeagueTableComponent } from './view-league-table/view-league-table.component';
+import { TeamComponent } from './team/team.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'ViewLeagueTable', component: ViewLeagueTableComponent},
+  { path: 'Team', component: TeamComponent}
+];
+
+
+// const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),
+    RouterModule
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
