3f79c6eaaebf024fd9fd9d557dfe87486fffe70a
Abdullah Alwahaibi <19574962@students.ltu.edu.au>
2021-05-08 21:35:35 +0400

    created view league and team , tip and data class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewLeagueTableComponent } from './view-league-table/view-league-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewLeagueTableComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/view-league-table/view-league-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-league-table',
+  templateUrl: './view-league-table.component.html',
+  styleUrls: ['./view-league-table.component.scss']
+})
+export class ViewLeagueTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
