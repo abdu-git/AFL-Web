1ed72d6b1bd1675d589e55c361314f4b24f7cdc0
Abdullah Alwahaibi <19574962@students.ltu.edu.au>
2021-05-09 06:22:03 +0400

    created a teams list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ViewLeagueTableComponent } from './view-league-table/view-league-table.component';
 import { TeamComponent } from './team/team.component';
+import { TeamResultComponent } from './team-result/team-result.component';
 
 const routes: Routes = [
   { path: 'ViewLeagueTable', component: ViewLeagueTableComponent},
-  { path: 'Team', component: TeamComponent}
+  { path: 'Team', component: TeamComponent},
+  { path: 'TeamResult', component:TeamResultComponent}
 ];
 
 @NgModule({
