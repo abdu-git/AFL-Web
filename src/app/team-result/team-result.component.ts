44b380feefa743579bea5dfeb8f1e8797c0ddd2c
Abdullah Alwahaibi <19574962@students.ltu.edu.au>
2021-05-11 01:09:53 +0400

    implemented user story 1

--- src/app/team-result/team-result.component.ts
+++ src/app/team-result/team-result.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
 
 import { Game } from '../game';
 import { Team } from '../team';
+import { Tip } from '../tip';
 import { DataService } from '../data.service';
 
 @Component({
@@ -12,16 +13,35 @@ import { DataService } from '../data.service';
 export class TeamResultComponent implements OnInit {
 
   games!:Game[];
+  tips!: Tip[];
   @Input() team!: Team;
+  // @Input() team!: Team;
   constructor(private dataService: DataService) { }
 
   ngOnChanges(changes: SimpleChanges) {
     if (changes['team']) {
-      this.getGames();
+      this.getTips();
     }
+    // if (changes['team']) {
+    //   this.getGames();
+    // }
   }
 
   ngOnInit(): void {
+  }
+  // user story 1
+  // As a fan, I want to see the prediction that my team will win their next game
+
+  getTips(): void {
+    this.dataService.getTips().subscribe(temp => { 
+      var tempArr: Tip[] = [];
+
+      temp.forEach(element => {
+        if(element.hteamid == this.team.id || element.ateamid == this.team.id) tempArr.push(element);
+      });
+      
+      this.tips = tempArr;    
+    });  
   }
 
   getGames(): void {
